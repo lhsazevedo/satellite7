4b357431947247b954e473ebd704fa92b22a3368
Lucas Azevedo <lhs_azevedo@hotmail.com>
2021-09-03 11:41:36 -0300

    Rename powerup constant

--- src/data/waves.asm
+++ src/data/waves.asm
@@ -29,13 +29,13 @@ enemy_wave 08  07 ENTITY_ENEMY_1
 
 enemy_wave 20  07 ENTITY_ENEMY_2
 
-enemy_wave 20  02 ENTITY_POWER_UP \
+enemy_wave 20  02 ENTITY_STAR \
                07 ENTITY_ENEMY_1
 
-enemy_wave 20  01 ENTITY_POWER_UP \
+enemy_wave 20  01 ENTITY_STAR \
                07 ENTITY_ENEMY_2
 
-enemy_wave 32  02 ENTITY_POWER_UP \
+enemy_wave 32  02 ENTITY_STAR \
                07 $0B
 
 enemy_wave 34  02 $1D \
@@ -44,20 +44,20 @@ enemy_wave 34  02 $1D \
 enemy_wave 32  01 $1D \
                07 $0B
 
-enemy_wave 20  02 ENTITY_POWER_UP \
+enemy_wave 20  02 ENTITY_STAR \
                07 $10
 
-enemy_wave 20  02 ENTITY_POWER_UP \
+enemy_wave 20  02 ENTITY_STAR \
                07 ENTITY_ENEMY_2
 
 enemy_wave 05  07 $0D
 
 enemy_wave 05  07 $0D
 
-enemy_wave 16  02 ENTITY_POWER_UP \
+enemy_wave 16  02 ENTITY_STAR \
                07 $1A
 
-enemy_wave 08  02 ENTITY_POWER_UP \
+enemy_wave 08  02 ENTITY_STAR \
                07 $1A
 
 enemy_wave 32  01 $1D \
@@ -66,10 +66,10 @@ enemy_wave 32  01 $1D \
 enemy_wave 16  06 $10 \
                07 ENTITY_ENEMY_1
 
-enemy_wave 12  02 ENTITY_POWER_UP \
+enemy_wave 12  02 ENTITY_STAR \
                07 $1A
 
-enemy_wave 22  01 ENTITY_POWER_UP \
+enemy_wave 22  01 ENTITY_STAR \
                07 ENTITY_ENEMY_1
 
 enemy_wave 32  01 $1C \
@@ -80,17 +80,17 @@ enemy_wave 20  07 $12 \
 
 enemy_wave 48  01 $11
 
-enemy_wave 20  02 ENTITY_POWER_UP \
+enemy_wave 20  02 ENTITY_STAR \
                07 $12
 
 enemy_wave 00  07 $0C
 
 enemy_wave 00  07 $0C
 
-enemy_wave 16  03 ENTITY_POWER_UP \
+enemy_wave 16  03 ENTITY_STAR \
                07 $15
 
-enemy_wave 16  02 ENTITY_POWER_UP \
+enemy_wave 16  02 ENTITY_STAR \
                07 $1A
 
 enemy_wave 05  07 $0D
@@ -109,7 +109,7 @@ enemy_wave 32  03 $1D \
 enemy_wave 24  01 $0E \
                07 ENTITY_ENEMY_1
 
-enemy_wave 24  02 ENTITY_POWER_UP \
+enemy_wave 24  02 ENTITY_STAR \
                07 $0B
 
 enemy_wave 32  02 $1D \
@@ -125,7 +125,7 @@ enemy_wave 12  07 ENTITY_ENEMY_1
 enemy_wave 08  07 $1B \
                07 $1A
 
-enemy_wave 18  04 ENTITY_POWER_UP \
+enemy_wave 18  04 ENTITY_STAR \
                07 $10
 
 enemy_wave 05  07 $0D
@@ -135,7 +135,7 @@ enemy_wave 00  07 $0C
 enemy_wave 16  07 $15 \
                07 $15
 
-enemy_wave 12  02 ENTITY_POWER_UP \
+enemy_wave 12  02 ENTITY_STAR \
                07 ENTITY_ENEMY_1
 
 enemy_wave 20  07 $12 \
@@ -150,7 +150,7 @@ enemy_wave 18  01 $0E \
 
 enemy_wave 12  07 $1A
 
-enemy_wave 08  02 ENTITY_POWER_UP \
+enemy_wave 08  02 ENTITY_STAR \
                07 $1A
 
 enemy_wave 20  07 $12 \
@@ -164,7 +164,7 @@ enemy_wave 24  04 $1C \
 
 enemy_wave 00  07 $13
 
-enemy_wave 16  03 ENTITY_POWER_UP \
+enemy_wave 16  03 ENTITY_STAR \
                07 $1A
 
 enemy_wave 64  01 $1D \
@@ -175,7 +175,7 @@ enemy_wave 64  01 $1D \
 
 enemy_wave 08  07 $1A
 
-enemy_wave 07  03 ENTITY_POWER_UP \
+enemy_wave 07  03 ENTITY_STAR \
                07 $1A
 
 enemy_wave 16  07 $15 \
@@ -183,7 +183,7 @@ enemy_wave 16  07 $15 \
 
 enemy_wave 05  07 $0D
 
-enemy_wave 16  02 ENTITY_POWER_UP \
+enemy_wave 16  02 ENTITY_STAR \
                07 ENTITY_ENEMY_1
 
 enemy_wave 05  07 $0D
@@ -193,7 +193,7 @@ enemy_wave 28  04 $1D \
 
 enemy_wave 64  01 $11
 
-enemy_wave 18  02 ENTITY_POWER_UP \
+enemy_wave 18  02 ENTITY_STAR \
                07 $10
 
 enemy_wave 28  04 $1D \
@@ -201,14 +201,14 @@ enemy_wave 28  04 $1D \
 
 enemy_wave 64  01 $11
 
-enemy_wave 18  03 ENTITY_POWER_UP \
+enemy_wave 18  03 ENTITY_STAR \
                07 $10
 
 enemy_wave 24  01 $1C \
                07 $15
 
-enemy_wave 34  07 ENTITY_POWER_UP \
-               01 ENTITY_POWER_UP
+enemy_wave 34  07 ENTITY_STAR \
+               01 ENTITY_STAR
 
 enemy_wave 08  07 $1A
 
@@ -263,7 +263,7 @@ enemy_wave 12  07 $0B \
                07 ENTITY_ENEMY_1
 
 enemy_wave 24  04 $1C \
-               03 ENTITY_POWER_UP
+               03 ENTITY_STAR
 
 enemy_wave 12  07 $12 \
                07 $1A
@@ -277,7 +277,7 @@ enemy_wave 16  07 $12
 enemy_wave 00  01 $11
 
 enemy_wave 32  02 $1C \
-               07 ENTITY_POWER_UP
+               07 ENTITY_STAR
 
 enemy_wave 00  00 $00
 
