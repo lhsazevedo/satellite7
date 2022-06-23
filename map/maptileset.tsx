ad0e3246c46714154371addc588ecccb004daf2a
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-06-23 01:53:17 -0300

    Add maptileset

---
+++ map/maptileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="maptileset" tilewidth="8" tileheight="8" tilecount="256" columns="32">
+ <image source="maptiles.png" width="256" height="64"/>
+</tileset>
